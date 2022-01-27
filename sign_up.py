1ec048311054690b1897fa1a4fdc8f1695a02583
DO VAN HIEP <39146260+hiepcute@users.noreply.github.com>
2022-01-28 01:50:42 +0700

    signup

---
+++ sign_up.py
@@ -0,0 +1,9 @@
+@app.route('/register', methods=['POST'])
+def signup_user():
+    data = request.get_json()
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+
+    new_user = Users(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'registered successfully'})
