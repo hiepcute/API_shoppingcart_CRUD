28a16cda7cf34682b49057079fafc86e116994ed
DO VAN HIEP <39146260+hiepcute@users.noreply.github.com>
2022-01-28 01:28:11 +0700

    commit message

---
+++ user.py
@@ -0,0 +1,7 @@
+from user import *
+class Users(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   public_id = db.Column(db.Integer)
+   name = db.Column(db.String(50))
+   password = db.Column(db.String(50))
+   admin = db.Column(db.Boolean)
