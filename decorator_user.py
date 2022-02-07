42ad5df26d60c7f3ee29bbe9a1869d4d883f9da3
DO VAN HIEP <39146260+hiepcute@users.noreply.github.com>
2022-02-07 13:50:02 +0700

    Update decorator_user.py

--- decorator_user.py
+++ decorator_user.py
@@ -3,17 +3,19 @@ def token_required(f):
     @wraps(f)
     def decorator(*args, **kwargs):
         token = None
+        # jwt is passed in the request header
         if 'x-access-tokens' in request.headers:
             token = request.headers['x-access-tokens']
-
+        # return 401 if token is not passed
         if not token:
             return jsonify({'message': 'a valid token is missing'})
         try:
+             # decoding the payload to fetch the stored details
             data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
             current_user = Users.query.filter_by(public_id=data['public_id']).first()
         except:
             return jsonify({'message': 'token is invalid'})
-
+        returns the current logged in users contex to the routes
         return f(current_user, *args, **kwargs)
 
     return decorator
